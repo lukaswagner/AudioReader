95451199471500ea467d32b1ade95630bc1d98a7
Lukas Wagner <lukas.wagner@student.hpi.uni-potsdam.de>
2017-07-26 17:39:44 +0200

    cleaned up

--- AudioReader/Src/Program.cs
+++ AudioReader/Src/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define USE_CONST_DEVICE
+
+using System;
 using System.Threading;
 using Un4seen.Bass;
 using Un4seen.Bass.Misc;
@@ -12,31 +14,28 @@ namespace AudioReader
         private static float[] _data;
         private static bool _dataValid = false;
         private static Visualization _vis;
-        private static BPMCounter _bpm;
-        //private static System.Timers.Timer _timer;
-        private static Timer _timer;
-        private static int _stream;
 
         static void Main(string[] args)
         {
-            //for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
-            //{
-            //    var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
-            //    if (device.IsEnabled && device.IsLoopback)
-            //    {
-            //        Console.WriteLine(string.Format("{0} - {1}", i, device.name));
-            //    }
-            //}
-
-            //Console.WriteLine("Enter target device id");
-            //String id = Console.ReadLine();
-            //int id_i = Int32.Parse(id);
+#if USE_CONST_DEVICE
             int id_i = 7;
+#else
+            for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
+            {
+                var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+                if (device.IsEnabled && device.IsLoopback)
+                {
+                    Console.WriteLine(string.Format("{0} - {1}", i, device.name));
+                }
+            }
+
+            Console.WriteLine("Enter target device id");
+            String id = Console.ReadLine();
+            int id_i = Int32.Parse(id);
+#endif
 
             _data = new float[2048];
-
-            //Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_UPDATETHREADS, true);
-            Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_UPDATEPERIOD, 0);
+            
             BASS_WASAPI_DEVICEINFO devInfo = BassWasapi.BASS_WASAPI_GetDeviceInfo(id_i);
             _callbackProcess = new WASAPIPROC(_callbackFunction);
 
@@ -44,55 +43,14 @@ namespace AudioReader
             _say("Bass_Init done");
             _checkError(BassWasapi.BASS_WASAPI_Init(id_i, devInfo.mixfreq, devInfo.mixchans, BASSWASAPIInit.BASS_WASAPI_BUFFER, 0f, 0f, _callbackProcess, IntPtr.Zero));
             _say("BASS_WASAPI_Init done");
-            //BASS_WASAPI_INFO info =  BassWasapi.BASS_WASAPI_GetInfo();
-            //_say("BASS_WASAPI_GetInfo done");
-            //_stream = Bass.BASS_StreamCreatePush(devInfo.mixfreq, devInfo.mixchans, BASSFlag.BASS_STREAM_DECODE | BASSFlag.BASS_SAMPLE_FLOAT, IntPtr.Zero);
-            //_say("BASS_StreamCreatePush done");
             _checkError(BassWasapi.BASS_WASAPI_Start());
             _say("BASS_WASAPI_Start done");
-
-            //BassWasapiHandler _wasapi = new BassWasapiHandler(7, false, devInfo.mixfreq, devInfo.mixchans, 0f, 0f);
-            //_wasapi.Init();
-            //_wasapi.SetFullDuplex(0, BASSFlag.BASS_STREAM_DECODE, false);
-            //int recordStream = _wasapi.InputChannel;
-            //_wasapi.Start();
 
             _vis = new Visualization(_data);
             _vis.Run();
 
-            //_bpm = new BPMCounter(25, devInfo.mixfreq);
-            //_bpm.BPMHistorySize = 2;
-            //_bpm.MaxBPM = 250;
-
-
-            //int beatCount = 0;
-            //_timer = new Timer((stateInfo) =>
-            //{
-            //    //_vis.Beat();
-            //    bool b = _bpm.ProcessAudio(_stream, true);
-            //    if (b)
-            //        Console.WriteLine(beatCount++);
-            //    //_vis.Beat();
-            //    //Console.WriteLine(_bpm.BPM);
-            //}, null, 0, 25);
-
-            //Thread t = new Thread(CreateTimer);
-            //t.Start();
-
-            //_vis = new Visualization(_data);
-            //_vis.Run();
-
             Thread.Sleep(-1);
-            //while (true) { }
         }
-
-        //public static void CreateTimer()
-        //{
-        //    _timer = new System.Timers.Timer();
-        //    _timer.Elapsed += _vis.Beat;
-        //    _timer.Interval = 4;
-        //    _timer.Enabled = true;
-        //}
 
         private static void _checkError(bool success)
         {
@@ -111,7 +69,6 @@ namespace AudioReader
         private static int _callbackFunction(IntPtr buffer, int length, IntPtr user)
         {
             _dataValid = BassWasapi.BASS_WASAPI_GetData(_data, (int)(BASSData.BASS_DATA_FFT2048) | (int)(BASSData.BASS_DATA_FFT_INDIVIDUAL)) >= 0;
-            //Bass.BASS_StreamPutData(_stream, buffer, length);
             return length;
         }
     }
--- AudioReader/Src/Visualization.cs
+++ AudioReader/Src/Visualization.cs
@@ -52,6 +52,8 @@ namespace AudioReader
             _setupWindow();
         }
 
+        #region helper
+
         private static double Clamp(double value, double min, double max)
         {
             return value < min ? min : value > max ? max : value;
@@ -73,21 +75,12 @@ namespace AudioReader
         {
             double floor = Math.Floor(index);
             double ceiling = Math.Ceiling(index);
-            //if(index<100) Console.WriteLine("{0} {1} {2}", index, floor, ceiling);
             if (floor == ceiling)
-            {
-                //if (index < 10) Console.WriteLine("{0} _ {1} _ {2} _ {3}", index, floor, ceiling, _getOffsettedValue(index, offset));
                 return _getOffsettedValue(index, offset);
-            }
-            //if (index < 10) Console.WriteLine("{0} _ {1} _ {2} _ {3}", index, floor, ceiling, MapClamped(index, floor, ceiling, _getOffsettedValue(floor, offset), _getOffsettedValue(ceiling, offset)));
             return MapClamped(index, floor, ceiling, _getOffsettedValue(floor, offset), _getOffsettedValue(ceiling, offset));
         }
 
-        public void Beat(object source, ElapsedEventArgs e)
-        {
-            GL.Color4(Color4.Red);
-            _beat = true;
-        }
+        #endregion helper
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
@@ -95,27 +88,19 @@ namespace AudioReader
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.Color4(Color4.Gray);
+            GL.LineWidth(2);
 
-            GL.LineWidth(5);
-
-            //if(_beat)
-            //{
-            //    GL.Color4(Color4.Red);
-            //    _beat = false;
-            //    return;
-            //}
-
+            // volume normalization
             double volumeMax = _maxVolume.Count > 0 ? _maxVolume.Max() : 0.01;
             double valueFactor = 0.8 / volumeMax;
-
             double maxVolume = 0;
 
+            // bass detection
             double bassSum = 0;
             for (int i = 0; i < 10; i++) bassSum += _data[i];
             if (_bassVolume.Count >= _bassSamples)
                 _bassVolume.Dequeue();
             _bassVolume.Enqueue(bassSum);
-
             if(bassSum > _bassVolume.Average() * 1.5) GL.Color4(Color4.White);
 
             // find base b so that b^(samples) = entries => logarithmic scaling on x-axis
@@ -125,17 +110,20 @@ namespace AudioReader
             double[] sampleEdges = new double[_samplesPerChannel + 1];
             for (int i = 0; i <= _samplesPerChannel; i++)
                 sampleEdges[i] = (int)Math.Pow(b, i) - 1;
-            //sampleEdges[i] = ((double)i / _samplesPerChannel) * _entriesPerChannel;
 
-            //Console.WriteLine(string.Join(" ", sampleEdges));
             int offset = 0;
 
             GL.Begin(PrimitiveType.Lines);
+            // max volume line
             GL.Vertex2(-1, volumeMax * valueFactor);
             GL.Vertex2(1, volumeMax * valueFactor);
+            // center line
             GL.Vertex2(0, 0);
             GL.Vertex2(0, 1);
-            //Console.WriteLine(volumeMax + " " + (volumeMax * valueFactor));
+            GL.End();
+
+            GL.LineWidth(5);
+            GL.Begin(PrimitiveType.Lines);
             for (int i = 0; i < _samplesPerChannel; i++)
             {
                 // sum up all values in range
@@ -149,24 +137,6 @@ namespace AudioReader
                     left += _interpolatedValue(j, 0);
                     right += _interpolatedValue(j, 1);
                 }
-                // normalize
-                //left /= Math.Ceiling(end - start);
-                //right /= Math.Ceiling(end - start);
-
-                //left += 1;
-                //right += 1;
-
-                //left = Math.Log(left);
-                //right = Math.Log(right);
-
-                //int samplePos = (int)(Math.Log(i * _entriesPerChannel / _samplesPerChannel, _samplesPerChannel) * _samplesPerChannel);
-                //Console.WriteLine(samplePos);
-                //int samplePos = i * _entriesPerChannel / _samplesPerChannel;
-
-                //double left = _data[samplePos * 2];
-                //double right = Math.Log(_data[samplePos * 2 + 1], volumeAvg);
-
-                //Console.WriteLine("{0} {1} {2}", left, right, volumeAvg);
 
                 if (left > maxVolume) maxVolume = left;
                 if (right > maxVolume) maxVolume = right;
@@ -184,13 +154,6 @@ namespace AudioReader
             if (_maxVolume.Count >= _volumeSamples)
                 _maxVolume.Dequeue();
             _maxVolume.Enqueue(maxVolume);
-
-            //GL.Begin(PrimitiveType.Quads);
-            //GL.Vertex2(0, 0);
-            //GL.Vertex2(1, 0);
-            //GL.Vertex2(1, 1);
-            //GL.Vertex2(0, 1);
-            //GL.End();
 
             SwapBuffers();
         }
