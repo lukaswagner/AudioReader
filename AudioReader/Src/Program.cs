dd78dfd51389cd47f6104b3c257776b5d451522e
Lukas Wagner <lukas.wagner@student.hpi.de>
2017-07-29 20:11:35 +0200

    reworked audio setup based on ini

--- AudioReader/Src/Program.cs
+++ AudioReader/Src/Program.cs
@@ -15,38 +15,16 @@ namespace AudioReader
 
         static void Main(string[] args)
         {
+            _data = new float[2048];
+
             IniParser.Load();
 
             Dictionary<string, string> audioConfig = IniParser.GetSectionParameter("audio");
-
             if (!audioConfig.TryGetValue("device", out string deviceId))
-            {
-                for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
-                {
-                    var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
-                    if (device.IsEnabled && device.IsLoopback)
-                    {
-                        Console.WriteLine(string.Format("{0} - {1}", i, device.name));
-                    }
-                }
+                deviceId = _listDevices();
 
-                Console.WriteLine("Enter target device id");
-                deviceId = Console.ReadLine();
-            }
-
-            int deviceId_int = Int32.Parse(deviceId);
-
-            _data = new float[2048];
-
-            BASS_WASAPI_DEVICEINFO devInfo = BassWasapi.BASS_WASAPI_GetDeviceInfo(deviceId_int);
-            _callbackProcess = new WASAPIPROC(_callbackFunction);
-
-            _checkError(Bass.BASS_Init(0, devInfo.mixfreq, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero));
-            _say("Bass_Init done");
-            _checkError(BassWasapi.BASS_WASAPI_Init(deviceId_int, devInfo.mixfreq, devInfo.mixchans, BASSWASAPIInit.BASS_WASAPI_BUFFER, 0f, 0f, _callbackProcess, IntPtr.Zero));
-            _say("BASS_WASAPI_Init done");
-            _checkError(BassWasapi.BASS_WASAPI_Start());
-            _say("BASS_WASAPI_Start done");
+            while(!_setUpAudio(deviceId))
+                deviceId = _listDevices();
 
             _vis = new Visualization(_data);
             _vis.Run();
@@ -54,13 +32,40 @@ namespace AudioReader
             Thread.Sleep(-1);
         }
 
-        private static void _checkError(bool success)
+        private static string _listDevices()
         {
-            if (!success)
+            for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
             {
-                Console.Write(Bass.BASS_ErrorGetCode());
-                Thread.Sleep(-1);
+                var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+                if (device.IsEnabled && device.IsLoopback)
+                {
+                    Console.WriteLine(string.Format("{0} - {1}", i, device.name));
+                }
             }
+
+            Console.WriteLine("Enter target device id");
+            return Console.ReadLine();
+        }
+
+        private static bool _setUpAudio(string deviceId)
+        {
+            int deviceId_int = Int32.Parse(deviceId);
+
+            BASS_WASAPI_DEVICEINFO devInfo = BassWasapi.BASS_WASAPI_GetDeviceInfo(deviceId_int);
+            _callbackProcess = new WASAPIPROC(_callbackFunction);
+
+            return _checkError(Bass.BASS_Init(0, devInfo.mixfreq, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero), "BASS_Init")
+                && _checkError(BassWasapi.BASS_WASAPI_Init(deviceId_int, devInfo.mixfreq, devInfo.mixchans, BASSWASAPIInit.BASS_WASAPI_BUFFER, 0f, 0f, _callbackProcess, IntPtr.Zero), "BASS_WASAPI_Init")
+                && _checkError(BassWasapi.BASS_WASAPI_Start(), "BASS_WASAPI_Start");
+        }
+
+        private static bool _checkError(bool success, string step)
+        {
+            if (success)
+                Console.WriteLine(step + " successful");
+            else
+                Console.Write(step + " unsuccessful. Error: " + Bass.BASS_ErrorGetCode());
+            return success;
         }
 
         private static void _say(object o)
