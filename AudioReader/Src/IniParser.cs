4d7e30c7ed492c69193e5c86c38985ad6ccbf12e
Lukas Wagner <lukas.wagner@student.hpi.de>
2017-07-29 19:53:55 +0200

    added ini parser, load audio device preset or list all if not available

---
+++ AudioReader/Src/IniParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioReader
+{
+    class IniParser
+    {
+        private static Dictionary<string, Dictionary<string, string>> _entries;
+
+        static IniParser()
+        {
+            _entries = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public static void Load(string filename = "config.ini")
+        {
+            // filter comments and empty lines
+            IEnumerable<string> lines = File.ReadLines(filename).Where((line) => !line.StartsWith("#") && !line.StartsWith(";") && line.Length > 0);
+
+            string section = "";
+
+            foreach(string line in lines)
+            {
+                // section
+                if (line.StartsWith("["))
+                {
+                    string trim = line.TrimStart('[');
+                    trim = trim.TrimEnd(']');
+                    trim = trim.Trim();
+                    trim = trim.ToLower();
+                    if (trim.Length == 0) Console.WriteLine("Invalid section in ini will be ignored: " + line);
+                    else section = trim;
+                    continue;
+                }
+
+                // property
+                string[] split = line.Split('=');
+
+                if(split.Length != 2 || split[0].Length == 0 || split[1].Length == 0)
+                {
+                    Console.WriteLine("Invalid parameter in ini will be ignored: " + line);
+                    continue;
+                }
+
+                if(section == "")
+                {
+                    Console.WriteLine("Cant set parameter without valid section: " + line);
+                    continue;
+                }
+
+                if (!_entries.TryGetValue(section, out Dictionary<string, string> sectionEntries))
+                    sectionEntries = new Dictionary<string, string>();
+
+                sectionEntries[split[0].ToLower().Trim()] = split[1].ToLower().Trim();
+                _entries[section] = sectionEntries;
+            }
+        }
+
+        public static Dictionary<string, string> GetSectionParameter(string section)
+        {
+            if (!_entries.TryGetValue(section, out Dictionary<string, string> sectionEntries))
+                sectionEntries = new Dictionary<string, string>();
+            return sectionEntries;
+        }
+    }
+}
--- AudioReader/Src/Program.cs
+++ AudioReader/Src/Program.cs
@@ -1,6 +1,5 @@
-﻿#define USE_CONST_DEVICE
-
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using Un4seen.Bass;
 using Un4seen.Bass.Misc;
@@ -17,31 +16,35 @@ namespace AudioReader
 
         static void Main(string[] args)
         {
-#if USE_CONST_DEVICE
-            int id_i = 7;
-#else
-            for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
+            IniParser.Load();
+
+            Dictionary<string, string> audioConfig = IniParser.GetSectionParameter("audio");
+
+            if (!audioConfig.TryGetValue("device", out string deviceId))
             {
-                var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
-                if (device.IsEnabled && device.IsLoopback)
+                for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
                 {
-                    Console.WriteLine(string.Format("{0} - {1}", i, device.name));
+                    var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+                    if (device.IsEnabled && device.IsLoopback)
+                    {
+                        Console.WriteLine(string.Format("{0} - {1}", i, device.name));
+                    }
                 }
+
+                Console.WriteLine("Enter target device id");
+                deviceId = Console.ReadLine();
             }
 
-            Console.WriteLine("Enter target device id");
-            String id = Console.ReadLine();
-            int id_i = Int32.Parse(id);
-#endif
+            int deviceId_int = Int32.Parse(deviceId);
 
             _data = new float[2048];
             
-            BASS_WASAPI_DEVICEINFO devInfo = BassWasapi.BASS_WASAPI_GetDeviceInfo(id_i);
+            BASS_WASAPI_DEVICEINFO devInfo = BassWasapi.BASS_WASAPI_GetDeviceInfo(deviceId_int);
             _callbackProcess = new WASAPIPROC(_callbackFunction);
 
             _checkError(Bass.BASS_Init(0, devInfo.mixfreq, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero));
             _say("Bass_Init done");
-            _checkError(BassWasapi.BASS_WASAPI_Init(id_i, devInfo.mixfreq, devInfo.mixchans, BASSWASAPIInit.BASS_WASAPI_BUFFER, 0f, 0f, _callbackProcess, IntPtr.Zero));
+            _checkError(BassWasapi.BASS_WASAPI_Init(deviceId_int, devInfo.mixfreq, devInfo.mixchans, BASSWASAPIInit.BASS_WASAPI_BUFFER, 0f, 0f, _callbackProcess, IntPtr.Zero));
             _say("BASS_WASAPI_Init done");
             _checkError(BassWasapi.BASS_WASAPI_Start());
             _say("BASS_WASAPI_Start done");
