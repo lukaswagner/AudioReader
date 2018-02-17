9e03d32b672f8a60809c3fe56621885a4f812dd9
Unknown <dajg1995@gmail.com>
2018-02-17 18:41:44 +0100

    Refs issue #10: parse destination ip from string

--- AudioReader/Src/ArtNetOutput.cs
+++ AudioReader/Src/ArtNetOutput.cs
@@ -18,6 +18,7 @@ namespace AudioReader
         private const uint SENDER_LENGTH = LED_COUNT * 3; // number of bytes to send
 
         private static ArtNetSocket s_artnet = new ArtNetSocket();
+        private static IPEndPoint endPoint;
         private static DateTime _lastTime = DateTime.Now; 
         private static Thread _artNetOutputLoopThread = new Thread(_artNetOutputLoop);
         private static bool _run = false;
@@ -34,6 +35,18 @@ namespace AudioReader
 
             s_artnet.EnableBroadcast = true;
             s_artnet.Open(lanAdress, IPAddress.Parse("255.255.255.0"));
+            
+            var destIPBytes = IPAddress.Parse("192.168.1.100").GetAddressBytes();
+            long destIP = 0;
+
+            long fac = 1;
+            foreach (var b in destIPBytes)
+            {
+                destIP += b * fac;
+                fac *= 0x100;
+            }
+
+            endPoint = new IPEndPoint(destIP, 6454);
 
             _run = true;
             _artNetOutputLoopThread.Start();
@@ -56,7 +69,7 @@ namespace AudioReader
                     for (uint i = 0; i < SENDER_LENGTH; i++) toSend.DmxData[i] = 0;
                     toSend.DmxData[(DateTime.Now.Second / 2) * 3] = 255;
 
-                    s_artnet.SendTo(toSend.ToArray(), new IPEndPoint(0x6401A8C0, 6454)); //reversed ip 192.168.1.100
+                    s_artnet.SendTo(toSend.ToArray(), endPoint);
                 }
             }
         }
