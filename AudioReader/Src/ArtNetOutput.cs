cc684f743ba86cb9fbc6201300156c890c2c4b8b
Unknown <dajg1995@gmail.com>
2018-02-17 18:06:22 +0100

    Refs issue #10: added test output for artnet

--- AudioReader/Src/ArtNetOutput.cs
+++ AudioReader/Src/ArtNetOutput.cs
@@ -1,6 +1,10 @@
+using ArtNet.Packets;
+using ArtNet.Sockets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,12 +13,28 @@ namespace AudioReader
 {
     class ArtNetOutput
     {
+        private const uint SENDER_UNIVERSES = 1; // number of universes to send
+        private const uint LED_COUNT = 30; // number of bytes to send
+        private const uint SENDER_LENGTH = LED_COUNT * 3; // number of bytes to send
+
+        private static ArtNetSocket s_artnet = new ArtNetSocket();
         private static DateTime _lastTime = DateTime.Now; 
         private static Thread _artNetOutputLoopThread = new Thread(_artNetOutputLoop);
         private static bool _run = false;
 
         public static void Enable()
         {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            IPAddress lanAdress = null;
+            foreach (var ip in localIPs)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork && ip.GetAddressBytes()[0] == 192)
+                    lanAdress = ip;
+            }
+
+            s_artnet.EnableBroadcast = true;
+            s_artnet.Open(lanAdress, IPAddress.Parse("255.255.255.0"));
+
             _run = true;
             _artNetOutputLoopThread.Start();
         }
@@ -25,8 +45,18 @@ namespace AudioReader
             {
                 if((DateTime.Now - _lastTime).Milliseconds > 16)
                 {
-                    //Log.Debug("ArtNet", "bla");
+                    _lastTime = DateTime.Now;
+                    ArtNetDmxPacket toSend = new ArtNetDmxPacket();
+                    Random rnd = new Random();
 
+                    toSend.DmxData = new byte[SENDER_LENGTH];
+
+                    toSend.Universe = (short)1;
+
+                    for (uint i = 0; i < SENDER_LENGTH; i++) toSend.DmxData[i] = 0;
+                    toSend.DmxData[(DateTime.Now.Second / 2) * 3] = 255;
+
+                    s_artnet.SendTo(toSend.ToArray(), new IPEndPoint(0x6401A8C0, 6454)); //reversed ip 192.168.1.100
                 }
             }
         }
